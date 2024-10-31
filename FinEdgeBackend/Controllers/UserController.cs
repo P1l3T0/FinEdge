de4accec475c6dbd339afe95df1e61f9d3863594
P1l3T0 <vgan3v@gmail.com>
2024-10-31 21:47:50 +0200

    feat(common): remoe unnecessary using statement closes P1l3T0/FinEdge#1

--- FinEdgeBackend/Controllers/UserController.cs
+++ FinEdgeBackend/Controllers/UserController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using FinEdgeBackend.DTOs;
 using FinEdgeBackend.Interfaces;
 using FinEdgeBackend.Models;
-using Microsoft.AspNetCore.Identity;
 
 namespace FinEdgeBackend.Controllers
 {
