a24220666efe77a7ea320d589c3670e12c5cd831
P1l3T0 <vgan3v@gmail.com>
2024-10-31 20:26:20 +0200

    feat(refreshtoken): add refresh token model

---
+++ FinEdgeBackend/Models/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinEdgeBackend.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Token { get; set; }
+        public bool IsRevoked { get; set; }
+        public int UserId { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public User? User { get; set; }
+    }
+}
