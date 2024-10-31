d27f8bfe814b2112606f2d57fdff55253b0f0939
P1l3T0 <vgan3v@gmail.com>
2024-10-31 20:39:06 +0200

    feat(interface):  add interfaces

---
+++ FinEdgeBackend/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using FinEdgeBackend.DTOs;
+using FinEdgeBackend.Models;
+
+namespace FinEdgeBackend.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<User> Register(RegisterDTO registerDto);
+        Task<User> Login(LoginDTO loginDto);
+    }
+}
