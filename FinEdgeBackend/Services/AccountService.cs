f1145ec4a324ccc30774c9998b32adcfb11d2b3e
P1l3T0 <vgan3v@gmail.com>
2024-11-01 23:41:09 +0200

    feat(account): add service crud methods

--- FinEdgeBackend/Services/AccountService.cs
+++ FinEdgeBackend/Services/AccountService.cs
@@ -1,6 +1,7 @@
 ﻿using FinEdgeBackend.Data;
 using FinEdgeBackend.Interfaces;
 using FinEdgeBackend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinEdgeBackend.Services
 {
@@ -8,11 +9,35 @@ namespace FinEdgeBackend.Services
     {
         private readonly DataContext _dataContext = dataContext;
 
-        public async Task<Account> CreateAsync(Account account)
+        public async Task<Account> CreateAccountAsync(Account account)
         {
-            _dataContext.Add(account);
+            _dataContext.Accounts.Add(account);
             await _dataContext.SaveChangesAsync();
             return account;
+        }
+
+        public async Task<Account> UpdateAccountAsync(int accountID, Account account)
+        {
+            _dataContext.Accounts.Update(account);
+            await _dataContext.SaveChangesAsync();
+            return account;
+        }
+
+        public async Task<Account> GetAccountById(int accountID)
+        {
+            Account? account = await _dataContext.Accounts!.FirstOrDefaultAsync(a => a.ID == accountID);
+            return account!;
+        }
+
+        public async Task<ICollection<Account>> GetAllAccountsForUserAsync(User currentUser)
+        {
+            ICollection<Account> accounts = await _dataContext.Accounts.Where(a => a.UserID == currentUser.ID).ToListAsync();
+            return accounts;
+        }
+
+        public void DeleteAccount(Account account)
+        {
+            throw new NotImplementedException();
         }
     }
 }
