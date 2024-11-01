1c2338b083357ebac4dbe6fdff2cbc354ca7c939
P1l3T0 <vgan3v@gmail.com>
2024-11-01 21:51:55 +0200

    feat(account): add model and dto

---
+++ FinEdgeBackend/DTOs/AccountDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FinEdgeBackend.DTOs
+{
+    public class AccountDTO
+    {
+        public string? Name { get; set; }
+        public string? AccountType { get; set; }
+        public decimal? Balance { get; set; }
+    }
+}
--- FinEdgeBackend/Models/Account.cs
+++ FinEdgeBackend/Models/Account.cs
@@ -2,12 +2,13 @@
 
 namespace FinEdgeBackend.Models
 {
-    public class Accounts
+    public class Account
     {
         public int ID { get; set; }
-        public int UserId { get; set; }
+        public int UserID { get; set; }
         public string? Name { get; set; }
+        public decimal? Balance { get; set; }
         public DateTime? DateCreated { get; set; }
-        public AccountType AccountType { get; set; }
+        public AccountType? AccountType { get; set; }
     }
 }
