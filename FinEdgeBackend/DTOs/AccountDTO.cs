f981bb4c1b791bdea0d1499c8759e0948d8dadd6
P1l3T0 <vgan3v@gmail.com>
2024-11-01 22:24:43 +0200

    refactor(dtos): update folder structure

--- FinEdgeBackend/DTOs/AccountDTO.cs
+++ FinEdgeBackend/DTOs/AccountDTO.cs
@@ -3,6 +3,7 @@
     public class AccountDTO
     {
         public string? Name { get; set; }
+        public string? Currency { get; set; }
         public string? AccountType { get; set; }
         public decimal? Balance { get; set; }
     }
