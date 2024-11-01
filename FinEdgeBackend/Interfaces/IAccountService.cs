cb76f638122197836e606899230fbebe28772a2f
P1l3T0 <vgan3v@gmail.com>
2024-11-01 23:40:39 +0200

    feat(account): add interface crud methods

--- FinEdgeBackend/Interfaces/IAccountService.cs
+++ FinEdgeBackend/Interfaces/IAccountService.cs
@@ -4,6 +4,10 @@ namespace FinEdgeBackend.Interfaces
 {
     public interface IAccountService
     {
-        Task<Account> CreateAsync(Account account);
+        Task<Account> CreateAccountAsync(Account account);
+        Task<Account> UpdateAccountAsync(int accountID, Account account);
+        Task<Account> GetAccountById(int accountID);
+        Task<ICollection<Account>> GetAllAccountsForUserAsync(User currentUser);
+        void DeleteAccount(Account account);
     }
 }
