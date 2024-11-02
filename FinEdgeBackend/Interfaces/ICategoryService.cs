e23863bb25ea4495f4531aca9f03e92f786b7cb1
P1l3T0 <vgan3v@gmail.com>
2024-11-02 15:36:15 +0200

    feat(category): add interface

---
+++ FinEdgeBackend/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using FinEdgeBackend.DTOs;
+using FinEdgeBackend.Models;
+
+namespace FinEdgeBackend.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<Category> CreateCategoryAsync(Category category);
+        Task<Category> UpdateCategoryAsync(CategoryDTO categoryDto, Category category);
+        Task<Category> GetCategoryById(int categoryID);
+        Task<ICollection<Category>> GetAllCategoriesForCurrentUser(User currentUser);
+        bool Validate(CategoryDTO categoryDto);
+        void DeleteCategory(Category category);
+    }
+}
