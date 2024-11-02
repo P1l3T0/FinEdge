6bc74e53c48fa25d7cbca944af427075a9df1a0e
P1l3T0 <vgan3v@gmail.com>
2024-11-02 15:30:38 +0200

    feat(category): add model and dto

---
+++ FinEdgeBackend/DTOs/CategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FinEdgeBackend.DTOs
+{
+    public class CategoryDTO
+    {
+        public string? Name { get; set; }
+        public string? Currency { get; set; }
+        public decimal? Balance { get; set; }
+        public decimal? Budget { get; set; }
+        public bool IsIncome { get; set; }
+    }
+}
---
+++ FinEdgeBackend/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace FinEdgeBackend.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string? Name { get; set; }
+        public string? Currency { get; set; }
+        public decimal? Balance { get; set; }
+        public decimal? Budget { get; set; }
+        public bool IsIncome { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
