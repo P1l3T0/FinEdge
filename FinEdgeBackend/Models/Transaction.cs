bd7c5c3672ceff8824928024a6e2c3a39199e458
P1l3T0 <vgan3v@gmail.com>
2024-11-03 21:30:40 +0200

    feat(transaction): add transaction model

---
+++ FinEdgeBackend/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FinEdgeBackend.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public int AccountID { get; set; }
+
+        [Required]
+        public int CategoryID { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        [JsonIgnore]
+        public Account? Account { get; set; }
+
+        [JsonIgnore]
+        public Category? Category { get; set; }
+    }
+}
