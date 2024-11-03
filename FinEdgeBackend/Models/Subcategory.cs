c99c14652d3f69915170eb49b32db295123afc5c
P1l3T0 <vgan3v@gmail.com>
2024-11-03 18:10:41 +0200

    feat(subcategory): add model anddatabase relation

---
+++ FinEdgeBackend/Models/Subcategory.cs
@@ -0,0 +1,10 @@
+﻿namespace FinEdgeBackend.Models
+{
+    public class Subcategory
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int? CategoryID { get; set; }
+        public Category? Category { get; set; }
+    }
+}
