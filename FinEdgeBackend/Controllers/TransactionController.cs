d5a1a94e97eb97fc72c3f8463a8f0c81f1c64768
P1l3T0 <vgan3v@gmail.com>
2024-11-04 22:01:54 +0200

    feat(transaction): add controller with create endpoint

---
+++ FinEdgeBackend/Controllers/TransactionController.cs
@@ -0,0 +1,50 @@
+﻿using FinEdgeBackend.DTOs;
+using FinEdgeBackend.Interfaces;
+using FinEdgeBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinEdgeBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController(ITransactionService transactionService ,ICategoryService categoryService, IAccountService accountService) : Controller
+    {
+        private readonly ITransactionService _transactionService = transactionService;
+        private readonly ICategoryService _categoryService = categoryService;
+        private readonly IAccountService _accountService = accountService;
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateTransaction([FromBody] TransactionDTO transactionDto)
+        {
+            if (!_transactionService.Validate(transactionDto))
+            {
+                return BadRequest("Error with the transactionDto fields");
+            }
+
+            Category category = await _categoryService.GetCategoryByNameAsync(transactionDto.CategoryName!);
+            Account account = await _accountService.GetAccountByNameAsync(transactionDto.AccountName!);
+            User currentUser = category.User!;
+
+            category.Balance += transactionDto.Amount;
+            account.Balance -= transactionDto.Amount;
+            currentUser.TotalBalance -= transactionDto.Amount;
+
+            await _transactionService.CreateTransactionAsync(new Transaction
+            {
+                Name = transactionDto.Name,
+                Amount = transactionDto.Amount,
+                UserID = currentUser.ID,
+                User = currentUser,
+                AccountID = account.ID,
+                AccountName = account.Name,
+                Account = account,
+                CategoryID = category.ID,
+                Category = category,
+                CategoryName = category.Name,
+            });
+
+            return Created();
+        }
+    }
+}
