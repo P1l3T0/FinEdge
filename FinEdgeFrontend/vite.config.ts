787297f567ba4977c81d6d425e8140ce9717f73c
P1l3T0 <vgan3v@gmail.com>
2024-11-09 17:43:52 +0200

    feat(frontend): create frontend project again cuz i screwed something

--- FinEdgeFrontend/vite.config.ts
+++ FinEdgeFrontend/vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.REACT_APP_API_URL': JSON.stringify(env.REACT_APP_API_URL)
-    },
-    plugins: [react()],
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
