6f1330b9426639f30f91c9d73d8d7823c9df5ad5
P1l3T0 <vgan3v@gmail.com>
2024-11-14 17:55:58 +0200

    refactor(account): improve get accounts

--- FinEdgeFrontend/src/Pages/Accounts/GetAccounts.tsx
+++ FinEdgeFrontend/src/Pages/Accounts/GetAccounts.tsx
@@ -1,13 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { getAllAccountsEndPoint } from '../../endpoints';
-import { Account } from '../../Utils/Types';
-import { Grid, GridColumn as Column } from '@progress/kendo-react-all';
+import { Account, AccountType } from '../../Utils/Types';
+import { Grid, GridColumn as Column, GridCellProps } from '@progress/kendo-react-all';
+import { getEnumValueFromNumber } from '../../Utils/Functions';
 
 const GetAccounts = () => {
-  const getAccounts = async (): Promise<Account[]> => {
+  const getAccounts = async () => {
     return await axios
-      .get(`${getAllAccountsEndPoint}`, { withCredentials: true })
+      .get<Account[]>(`${getAllAccountsEndPoint}`, { withCredentials: true })
       .then((res: AxiosResponse<Account[]>) => res.data)
       .catch((err: AxiosError) => {
         throw new Error(`No accounts found ${err.message}`);
@@ -24,6 +25,12 @@ const GetAccounts = () => {
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error: {error.message}</p>;
 
+  const AccountTypeCell = (props: GridCellProps) => {
+    const accountTypeValue: number = props.dataItem[props.field || ''];
+    const accountTypeLabel: string = getEnumValueFromNumber(accountTypeValue, AccountType);
+    return <td>{accountTypeLabel}</td>;
+  };
+
   return (
     <>
       <Grid data={data} style={{ width: "820px", height: "auto" }}>
@@ -32,7 +39,7 @@ const GetAccounts = () => {
         <Column field="name" title="Name" width="100px" />
         <Column field="balance" title="Balance" width="100px" />
         <Column field="currency" title="Currency" width="100px" />
-        <Column field="accountType" title="Account Type" width="125px" />
+        <Column field="accountType" title="Account Type" width="125px" cell={AccountTypeCell} />
         <Column field="dateCreated" title="Date Created" width="250px" />
       </Grid>
     </>
