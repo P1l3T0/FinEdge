35561f7e955c56b817155bec6747a75a6f309c8a
P1l3T0 <vgan3v@gmail.com>
2024-11-14 19:40:54 +0200

    refactor(home): split everything into components and remove user props

--- FinEdgeFrontend/src/Pages/Home.tsx
+++ FinEdgeFrontend/src/Pages/Home.tsx
@@ -1,30 +1,19 @@
-import { User } from "../Utils/Types";
+import GetUser from "../User/GetUser";
+import CreateAccounts from "./Accounts/CreateAccounts";
 import GetAccounts from "./Accounts/GetAccounts";
 
-const Home = ({ user }: { user: User, }) => {
+const Home = () => {
 
 
   return (
     <>
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Email</th>
-            <th>Methodology</th>
-          </tr>
-          <tr>
-            <td>{user?.name}</td>
-            <td>{user?.surname}</td>
-            <td>{user?.email}</td>
-            <td>{user?.methodologyType}</td>
-          </tr>
-        </tbody>
-      </table>
+      <h2> User</h2 >
+      <GetUser />
 
       <h2>Accounts</h2>
       <GetAccounts />
+      <br />
+      <CreateAccounts />
     </>
   )
 }
