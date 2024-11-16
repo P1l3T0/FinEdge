5a5cb98882a2a36eae497a1d1eaab3692ce4b111
P1l3T0 <vgan3v@gmail.com>
2024-11-16 15:28:33 +0200

    feat(category): add get expenditurecategories component

---
+++ FinEdgeFrontend/src/Pages/Categories/Categories.tsx
@@ -0,0 +1,15 @@
+import CreateCategory from './CreateCategory'
+import GetExpenditureCategories from './GetExpenditureCategories'
+
+const Categories = () => {
+
+
+  return (
+    <>
+      <CreateCategory />
+      <GetExpenditureCategories />
+    </>
+  )
+}
+
+export default Categories
