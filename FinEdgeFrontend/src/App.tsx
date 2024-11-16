8da386606f0f84f88dc76ce3b0800db23483fe7a
P1l3T0 <vgan3v@gmail.com>
2024-11-16 10:00:29 +0200

    refactor(home): remove accounts from home page

--- FinEdgeFrontend/src/App.tsx
+++ FinEdgeFrontend/src/App.tsx
@@ -4,8 +4,7 @@ import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import Logout from './Pages/Authentication/Logout';
 import Login from './Pages/Authentication/Login';
-import CreateAccounts from './Pages/Accounts/CreateAccounts';
-import GetAccounts from './Pages/Accounts/GetAccounts';
+import Accounts from './Pages/Accounts/Accounts';
 
 function App() {
   return (
@@ -15,8 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/accounts" element={<CreateAccounts />} />
-          <Route path="/accounts/get" element={<GetAccounts />} />
+          <Route path="/accounts" element={<Accounts />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
--- FinEdgeFrontend/src/Pages/Home.tsx
+++ FinEdgeFrontend/src/Pages/Home.tsx
@@ -1,6 +1,4 @@
 import GetUser from "../User/GetUser";
-import CreateAccounts from "./Accounts/CreateAccounts";
-import GetAccounts from "./Accounts/GetAccounts";
 
 const Home = () => {
 
@@ -9,11 +7,6 @@ const Home = () => {
     <>
       <h2> User</h2 >
       <GetUser />
-
-      <h2>Accounts</h2>
-      <GetAccounts />
-      <br />
-      <CreateAccounts />
     </>
   )
 }
