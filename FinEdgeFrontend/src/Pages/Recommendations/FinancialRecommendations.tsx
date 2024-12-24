5b2647fb91ff01c12230191fe43d2c465770a607
P1l3T0 <vgan3v@gmail.com>
2024-12-24 22:18:13 +0200

    refactor(financialrecommendations): remove get component

--- FinEdgeFrontend/src/Pages/Recommendations/FinancialRecommendations.tsx
+++ FinEdgeFrontend/src/Pages/Recommendations/FinancialRecommendations.tsx
@@ -1,13 +1,11 @@
 import CreateFinancialRecommendations from "./CreateFinancialRecommendations";
 import DeleteFinancialRecommendations from "./DeleteFinancialRecommendations";
-import GetFinancialRecommendations from "./GetFinancialRecommendations";
 
 const FinancialRecommendations = () => {
   return (
     <>
       <CreateFinancialRecommendations />
       <DeleteFinancialRecommendations />
-      <GetFinancialRecommendations />
     </>
   )
 }
