5887fae816b0550d93736349b295e498fd1b62b0
Vasil Ganev <vgan3v@gmail.com>
2025-04-27 15:49:08 +0300

    refactor(accounts): extract aiprompt logic to custom hook

--- FinEdgeFrontend/src/Components/AccountsPage/AIPrompt/AccountAIPrompt.tsx
+++ FinEdgeFrontend/src/Components/AccountsPage/AIPrompt/AccountAIPrompt.tsx
@@ -1,88 +1,55 @@
 import { useState } from "react";
-import axios, { AxiosError } from "axios";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AIPrompt, AIPromptView, AIPromptOutputView, promptViewDefaults, outputViewDefaults, AIPromptOutputInterface } from "@progress/kendo-react-conversational-ui";
 import useGetAccountPromptSuggestions from "../../../Hooks/FinancialRecommendations/useGetAccountPromptSuggestions";
-import { AccountRecommendation, PromptRequestData } from "../../../Utils/Types";
-import { createAccountRecommendationEndPoint } from "../../../Utils/endpoints";
+import useGenerateRecommendation from "../../../Hooks/Accounts/useGenerateAccountSuggestions";
 
 const AccountAIPrompt = () => {
-    const queryClient = useQueryClient();
-    const [promptRequestData, setPromptRequestData] = useState<PromptRequestData>({
-      prompt: "",
-      dateString: `${new Date().getDate()}-${new Date().getMonth() + 1}-${new Date().getFullYear()}`
-    });
-    
-    const { data, isLoading, isError, error } = useGetAccountPromptSuggestions();
+  const { data, isLoading, isError, error } = useGetAccountPromptSuggestions();
+  const { generateFinancialRecommendation } = useGenerateRecommendation();
 
-    const [activeView, setActiveView] = useState<string>(promptViewDefaults.name);
-    const [outputs, setOutputs] = useState<AIPromptOutputInterface[]>([]);
+  const [activeView, setActiveView] = useState<string>(promptViewDefaults.name);
+  const [outputs, setOutputs] = useState<AIPromptOutputInterface[]>([]);
 
-    const handleActiveViewChange = (viewName: string) => {
-      setActiveView(viewName);
-    };
+  const handleActiveViewChange = (viewName: string) => {
+    setActiveView(viewName);
+  };
 
-    const handlePromptRequest = (prompt?: string) => {
-      if (!prompt) {
-        return;
-      }
-
-      const recommendation = generateFinancialRecommendation(prompt);
-
-      recommendation
-        .then((data) => {
-          setOutputs([
-            {
-              id: data.id,
-              title: prompt,
-              responseContent: data?.responseContent!,
-              prompt,
-            },
-            ...outputs,
-          ]);
-
-          setActiveView(outputViewDefaults.name);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-
-    const generateFinancialRecommendation = async (prompt: string): Promise<AccountRecommendation> => {
-      setPromptRequestData({
-        ...promptRequestData,
-        prompt: prompt,
-      });
-      
-      return await mutateAsync();
+  const handlePromptRequest = (prompt?: string) => {
+    if (!prompt) {
+      return;
     }
 
-    const createRecommendations = async () => {
-      const response = await axios
-        .post(createAccountRecommendationEndPoint, promptRequestData, { withCredentials: true })
-        .catch((error: AxiosError) => {});
+    const recommendation = generateFinancialRecommendation(prompt);
 
-      return response?.data as AccountRecommendation;
-    };
+    recommendation.then((data) => {
+        setOutputs([{
+            id: data.id,
+            title: prompt,
+            responseContent: data?.responseContent!,
+            prompt,
+          },
+          ...outputs,
+        ]);
 
-    const { mutateAsync } = useMutation({
-      mutationFn: createRecommendations,
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["account-recommendations"] });
-      },
-    });
+        setActiveView(outputViewDefaults.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>Error: {error!.message}</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error!.message}</p>;
 
-    return (
-      <>
-        <AIPrompt className="px-5 w-70 sm:w-90 md:w-139 shadow-sm" activeView={activeView} onActiveViewChange={handleActiveViewChange} onPromptRequest={handlePromptRequest}>
-          <AIPromptView promptSuggestions={data} />
-          <AIPromptOutputView outputs={outputs} showOutputRating={true} />
-        </AIPrompt>
-      </>
-    );
+  return (
+    <>
+      <AIPrompt className="px-5 w-70 sm:w-90 md:w-139 shadow-sm" activeView={activeView}
+        onActiveViewChange={handleActiveViewChange} onPromptRequest={handlePromptRequest}>
+        <AIPromptView promptSuggestions={data} />
+        <AIPromptOutputView outputs={outputs} showOutputRating={true} />
+      </AIPrompt>
+    </>
+  );
 };
 
 export default AccountAIPrompt;
