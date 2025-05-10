98b1912a3c28f821f2888e5ed7dc8327e921785f
Vasil Ganev <vgan3v@gmail.com>
2025-05-10 13:00:14 +0300

    feat(transactions): add listview to transaction page

--- FinEdgeFrontend/src/Components/TransactionsPage/CRUD/GetTransactions.tsx
+++ FinEdgeFrontend/src/Components/TransactionsPage/CRUD/GetTransactions.tsx
@@ -1,21 +1,40 @@
 import useGetTransactions from "../../../Hooks/Transactions/useGetTransactions";
-import TransactionCards from "../Cards/TransactionCard/TransactionCards";
 import { filterBy, CompositeFilterDescriptor } from "@progress/kendo-data-query";
 import TransactionFilter from "./TransactionFilter";
 import { useState } from "react";
 import TransactionFAB from "../AIPrompt/TransactionFAB";
+import { Pager, PageChangeEvent } from "@progress/kendo-react-data-tools";
+import { ListView } from "@progress/kendo-react-listview";
+import TransactionCard from "../Cards/TransactionCard/TransactionCard";
 
 const GetTransactions = () => {
+  const { data, isLoading, isError, error } = useGetTransactions();
+
   const [filter, setFilter] = useState<CompositeFilterDescriptor>({
     logic: "and",
     filters: [],
   });
 
-  const { data, isLoading, isError, error } = useGetTransactions();
+  const [page, setPage] = useState({
+    skip: 0,
+    take: 10,
+  });
 
   const handleFilterChange = (newFilter: CompositeFilterDescriptor) => {
     setFilter(newFilter);
   };
+
+  const handlePageChange = (e: PageChangeEvent) => {
+    setPage({
+      skip: e.skip,
+      take: e.take,
+    });
+  };
+
+  const { skip, take } = page;
+
+  const filteredData = filterBy(data?.allTransactions || [], filter);
+  const pagedData = filteredData.slice(skip, skip + take);
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error: {error?.message}</p>;
@@ -23,13 +42,8 @@ const GetTransactions = () => {
   return (
     <>
       <TransactionFilter onFilterChange={handleFilterChange} />
-      <div className="flex flex-col lg:flex-row gap-4">
-        <div className="flex-grow">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
-            <TransactionCards transactions={filterBy(data?.allTransactions!, filter)} />
-          </div>
-        </div>
-      </div>
+      <ListView data={pagedData} item={TransactionCard} />
+      <Pager className="k-listview-pager" skip={skip} take={take} onPageChange={handlePageChange} total={data?.allTransactions.length!} />
 
       <TransactionFAB />
     </>
--- FinEdgeFrontend/src/Components/TransactionsPage/Cards/TransactionCard/TransactionCard.tsx
+++ FinEdgeFrontend/src/Components/TransactionsPage/Cards/TransactionCard/TransactionCard.tsx
@@ -3,12 +3,15 @@ import TransactionCardFooter from "./TransactionCardFooter";
 import TransactionCardBody from "./TransactionCardBody";
 import TransactionCardHeader from "./TransactionCardHeader";
 import { Card, CardHeader, CardBody, CardFooter } from "@progress/kendo-react-layout";
+import { ListViewItemProps } from "@progress/kendo-react-listview";
 
-const TransactionCards = ({ transactions }: { transactions: Transaction[] }) => {
+const TransactionCard = (props: ListViewItemProps) => {
+  const transaction: Transaction = props.dataItem;
+
   return (
     <>
-      {transactions?.map((transaction, index) => (
-        <Card key={index} className="shadow-md hover:shadow-xl duration-300 ease-in-out">
+      <div className="m-4">
+        <Card key={transaction.id} className="shadow-md hover:shadow-xl duration-300 ease-in-out w-70 h-90">
           <CardHeader>
             <TransactionCardHeader transaction={transaction} />
           </CardHeader>
@@ -19,9 +22,9 @@ const TransactionCards = ({ transactions }: { transactions: Transaction[] }) => 
             <TransactionCardFooter transaction={transaction} />
           </CardFooter>
         </Card>
-      ))}
+      </div>
     </>
   );
 }
 
-export default TransactionCards;
+export default TransactionCard;
